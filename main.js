d150ae802ebc42826c1c05e4671cf0920ebd56f6
BmElectro <bm.shadow@hotmail.com>
2022-06-06 01:39:28 +0300

    trying again

--- main.js
+++ main.js
@@ -1,50 +1,66 @@
-import fetch from 'node-fetch';
-import cors from 'cors'
-import express from 'express'
+const express = require("express");
+const app = express();
+
+app.use(express.json({ extended: false }));
+const axios = require('axios').default;
 
 
-let app = express()
-
-
-app.use(cors())
-app.use(express.json());
 
 
 const port = 8080
 const host = '127.0.0.1'
-
+const apiKey = process.env.APIKEY
 try {
-    
-    const apiKey = process.env.APIKEY
-    console.log(apiKey)
-    const challengesIdsUrl = `https://eun1.api.riotgames.com/lol/challenges/v1/challenges/config?api_key=${apiKey}` 
-    const challengesIdsTemp = await fetch(challengesIdsUrl);
-    const challengesIds = await challengesIdsTemp.json();
+    async function getSummoner(){
+    // const urlSummoner = ``
+    // const summonerTemp = await fetch(urlSummoner);
+    // const summoner = await summonerTemp.json();
+        const summoner = axios.get(`https://eun1.api.riotgames.com/lol/summoner/v4/summoners/by-name/KenrouHolo?api_key=${apiKey}`)
+        console.log(summoner)
+        return summoner
+    }
+    async function getChallengesIds(){
+        const challengesIds = axios.get(`https://eun1.api.riotgames.com/lol/challenges/v1/challenges/config?api_key=${apiKey}`)
+        console.log(challengesIds)
+        return challengesIds
+    }
+    async function getChallenges(summoner){
+        const challengesIds = axios.get(`https://eun1.api.riotgames.com/lol/challenges/v1/player-data/${summoner.puuid}?api_key=${apiKey}`)
+        console.log(challengesIds)
+        return challengesIds
+    }
+    // 
+    // console.log(apiKey)
+    // const challengesIdsUrl = `https://eun1.api.riotgames.com/lol/challenges/v1/challenges/config?api_key=${apiKey}` 
+    // const challengesIdsTemp = await fetch(challengesIdsUrl);
+    // const challengesIds = await challengesIdsTemp.json();
 
-    const urlSummoner = `https://eun1.api.riotgames.com/lol/summoner/v4/summoners/by-name/KenrouHolo?api_key=${apiKey}`
-    const summonerTemp = await fetch(urlSummoner);
-    const summoner = await summonerTemp.json();
+   
 
 
 
-    const challengesUrl = `https://eun1.api.riotgames.com/lol/challenges/v1/player-data/${summoner.puuid}?api_key=${apiKey}`
-    const challengesTemp = await fetch(challengesUrl);
-    const challenges = await challengesTemp.json();
+    // const challengesUrl = `https://eun1.api.riotgames.com/lol/challenges/v1/player-data/${summoner.puuid}?api_key=${apiKey}`
+    // const challengesTemp = await fetch(challengesUrl);
+    // const challenges = await challengesTemp.json();
 
 
-    const [my] = challengesIds.filter(e => e.id == 502003)
+    // const [my] = challengesIds.filter(e => e.id == 502003)
 
-    
+    console.log('blabla')
     app.get("/", function (req, res) {
         res.send('bla bla bla')
     })
-    app.get("/getName", function (req, res) {
-        res.send(JSON.stringify(my.localizedNames.en_US))
+    app.get("/getname", function (req, res) {
+        const ids = await getChallengesIds()
+        const summoner = await getSummoner()
+        const challenges = await getChallenges(summoner)
+
+        res.send(JSON.stringify(challenges))
     })
     
-    app.get("/api/getName", function (req, res) {
-        res.send(JSON.stringify(my.localizedNames.en_US))
-    })
+    // app.get("/api/getName", function (req, res) {
+    //     res.send(JSON.stringify(my.localizedNames.en_US))
+    // })
     
 
 } catch (error) {
