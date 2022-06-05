2c054182180b322102692058fdafc936d1b651d9
BmElectro <bm.shadow@hotmail.com>
2022-06-06 00:08:27 +0300

    try catch added engine to vercel

--- main.js
+++ main.js
@@ -13,29 +13,33 @@ app.use(express.json());
 const port = 8080
 const host = '127.0.0.1'
 
+try {
+    
+    const apiKey = process.env.APIKEY
+
+    const challengesIdsUrl = `https://eun1.api.riotgames.com/lol/challenges/v1/challenges/config?api_key=${apiKey}` 
+    const challengesIdsTemp = await fetch(challengesIdsUrl);
+    const challengesIds = await challengesIdsTemp.json();
+
+    const urlSummoner = `https://eun1.api.riotgames.com/lol/summoner/v4/summoners/by-name/KenrouHolo?api_key=${apiKey}`
+    const summonerTemp = await fetch(urlSummoner);
+    const summoner = await summonerTemp.json();
 
 
 
-const apiKey = process.env.APIKEY
-
-const challengesIdsUrl = `https://eun1.api.riotgames.com/lol/challenges/v1/challenges/config?api_key=${apiKey}` 
-const challengesIdsTemp = await fetch(challengesIdsUrl);
-const challengesIds = await challengesIdsTemp.json();
-
-const urlSummoner = `https://eun1.api.riotgames.com/lol/summoner/v4/summoners/by-name/KenrouHolo?api_key=${apiKey}`
-const summonerTemp = await fetch(urlSummoner);
-const summoner = await summonerTemp.json();
+    const challengesUrl = `https://eun1.api.riotgames.com/lol/challenges/v1/player-data/${summoner.puuid}?api_key=${apiKey}`
+    const challengesTemp = await fetch(challengesUrl);
+    const challenges = await challengesTemp.json();
 
 
+    const [my] = challengesIds.filter(e => e.id == 502003)
 
-const challengesUrl = `https://eun1.api.riotgames.com/lol/challenges/v1/player-data/${summoner.puuid}?api_key=${apiKey}`
-const challengesTemp = await fetch(challengesUrl);
-const challenges = await challengesTemp.json();
+} catch (error) {
+    console.log('err')
+}
 
 
-const [my] = challengesIds.filter(e => e.id == 502003)
 
-console.log(my.localizedNames.en_US);
 
 
 app.get("/", function (req, res) {
