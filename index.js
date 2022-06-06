d9041c9062c0fd398341c73415bc8b084357ec0d
BmElectro <bm.shadow@hotmail.com>
2022-06-06 03:08:59 +0300

    added functionality and a new route

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-//const product = require("./api/product");
+require('dotenv').config()
+
 
 
 app.use(express.json({ extended: false }));
@@ -10,79 +11,84 @@ const axios = require('axios').default;
 
 
 const port = 8080
-const host = '127.0.0.1'
 const apiKey = process.env.APIKEY
-try {
-    async function getSummoner(){
-    // const urlSummoner = ``
-    // const summonerTemp = await fetch(urlSummoner);
-    // const summoner = await summonerTemp.json();
+
+async function getSummoner(summonerName){
     try {
-        const summoner = await axios.get(`https://eun1.api.riotgames.com/lol/summoner/v4/summoners/by-name/KenrouHolo?api_key=${apiKey}`)
+        const summoner = await axios.get(`https://eun1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${apiKey}`)
         console.log(summoner)
-        return summoner
+        return summoner.data
     } catch (error) {
-        return 'err'
+        return error
     }
-
-    }
-    async function getChallengesIds(){
-
-        try {
-            const challengesIds = await axios.get(`https://eun1.api.riotgames.com/lol/challenges/v1/challenges/config?api_key=${apiKey}`)
-            console.log(challengesIds)
-            return challengesIds
-        } catch (error) {
-            return 'err'
-        }
-    }
-    async function getChallenges(summoner){
-
-        try {
-            const challengesIds = await axios.get(`https://eun1.api.riotgames.com/lol/challenges/v1/player-data/${summoner.puuid}?api_key=${apiKey}`)
-            console.log(challengesIds)
-            return challengesIds
-        } catch (error) {
-            return 'err'
-        }
-    }
-    // 
-    // console.log(apiKey)
-    // const challengesIdsUrl = `https://eun1.api.riotgames.com/lol/challenges/v1/challenges/config?api_key=${apiKey}` 
-    // const challengesIdsTemp = await fetch(challengesIdsUrl);
-    // const challengesIds = await challengesIdsTemp.json();
-
-   
-
-
-
-    // const challengesUrl = `https://eun1.api.riotgames.com/lol/challenges/v1/player-data/${summoner.puuid}?api_key=${apiKey}`
-    // const challengesTemp = await fetch(challengesUrl);
-    // const challenges = await challengesTemp.json();
-
-
-    // const [my] = challengesIds.filter(e => e.id == 502003)
-
-    //app.use("/api/product", product);
-    app.get("/", function (req, res) {
-        res.send('bla bla bla')
-    })
-    app.get("/getname", async function (req, res) {
-        const ids = await getChallengesIds()
-        const summoner = await getSummoner()
-        const challenges = await getChallenges(summoner)
-
-        res.send(JSON.stringify(challenges))
-    })
-    
-    // app.get("/api/getName", function (req, res) {
-    //     res.send(JSON.stringify(my.localizedNames.en_US))
-    // })
-    
-
-} catch (error) {
-    console.log('err')
 }
+async function getChallengesIds(){
+    try {
+        const challengesIds = await axios.get(`https://eun1.api.riotgames.com/lol/challenges/v1/challenges/config?api_key=${apiKey}`)
+        
+        return challengesIds.data
+    } catch (error) {
+        return error
+    }
+}
+async function getChallenges(summoner){
+    try {
+        const challengesIds = await axios.get(`https://eun1.api.riotgames.com/lol/challenges/v1/player-data/${summoner.puuid}?api_key=${apiKey}`)
+        
+        return challengesIds.data
+    } catch (error) {
+        return error
+    }
+}
+function convertTime(unix_timestamp){
+
+    let date = new Date(unix_timestamp);
+   
+    let hours = date.getHours();
+    
+    let minutes = date.getMinutes();
+    let year = date.getFullYear()
+    let month = date.getMonth()
+    let day = date.getDate()
+    let seconds = date.getSeconds();
+   
+    let formattedTime = hours + ':' + minutes + ':' + seconds;
+
+
+}
+
+
+
+app.get("/", function (req, res) {
+    res.send('bla bla bla')
+})
+app.get("/getchallenges", async function (req, res) {
+
+    const summonerName = req.query.sumname
+
+    const ids = await getChallengesIds()
+    const summoner = await getSummoner(summonerName)
+    const challenges = await getChallenges(summoner)
+    
+
+        ///el => el.challengeId = ids[el.challengeId].localizedNames.en_US
+    let challengesWithNames = []
+    for (chall of challenges.challenges){
+        const [challengeNameString] = ids.filter(e => e.id == chall.challengeId)
+
+        //console.log(challengeNameString.localizedNames.en_US)
+        chall.challengeId = challengeNameString.localizedNames.en_US.name
+        challengesWithNames.push(chall)
+    } 
+    challenges.challenges = challengesWithNames
+    
+    
+    res.send(JSON.stringify(challenges))
+})
+
+// app.get("/api/getName", function (req, res) {
+//     res.send(JSON.stringify(my.localizedNames.en_US))
+// })
 
 
 
